aaf1e56f3721d95c03e8756aa21a95c200d1ac14
Mohamed Sheikh <mysheikh08@gmail.com>
2017-06-06 18:41:25 -0400

    Fixed issue with php file for hero section and worked on contact page

---
+++ web/modules/custom/proj_me_core/src/Plugin/Block/ContactInformation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\proj_me_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'ContactInformation' block.
+ *
+ * @Block(
+ *  id = "contact_information_block",
+ *  admin_label = @Translation("Contact Information Block"),
+ * )
+ */
+class ContactInformation extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['contact_information_block']['#markup'] = '<div class="contact-box"> 
+    	<p>Email | <span class="contact-info"> mysheikh08@gmail.com</span></p>
+    	<p>Skype | <span class="contact-info"> m.sheikh2</span></p>
+    	<p>Phone | <span class="contact-info"> (571) 294-4768</span></p>
+    	<p>Linedin | <span class="contact-info"> Resume</span></p>
+    </div>';
+
+    return $build;
+  }
+
+}
