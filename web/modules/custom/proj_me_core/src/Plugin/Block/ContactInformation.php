aca546797e60dcf72f2ca2505c864778e957b237
Mohamed Sheikh <mysheikh08@gmail.com>
2017-08-15 13:34:10 -0400

    Styled the Portfolio page

--- web/modules/custom/proj_me_core/src/Plugin/Block/ContactInformation.php
+++ web/modules/custom/proj_me_core/src/Plugin/Block/ContactInformation.php
@@ -23,7 +23,7 @@ class ContactInformation extends BlockBase {
     	<p>Email | <span class="contact-info"> mysheikh08@gmail.com</span></p>
     	<p>Skype | <span class="contact-info"> m.sheikh2</span></p>
     	<p>Phone | <span class="contact-info"> (571) 294-4768</span></p>
-    	<p>Linedin | <span class="contact-info"> Resume</span></p>
+    	<p>Linkedin | <span class="contact-info"> Resume</span></p>
     </div>';
 
     return $build;
